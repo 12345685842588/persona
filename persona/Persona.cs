8842547166180508e5073c274a70bc92570a2a2c
Utente <Utente@DESKTOP-BAB4AAS>
2024-01-24 15:53:18 +0100

    Aggiungere i file di progetto.

---
+++ persona/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persona
+{
+    internal class Persona
+    {
+        public int AnnoDiNascita { get; set; }
+
+        public Persona(int annoDiNascita)
+        {
+            this.AnnoDiNascita = annoDiNascita;
+        }
+
+        public int CalcolaEtà()
+        {
+            return DateTime.Now.Year - AnnoDiNascita;
+        }
+    }
+}
---
+++ persona/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona(2005);
+
+            Console.WriteLine("età:{0}", p.CalcolaEtà());
+
+            Console.ReadLine();
+        }
+    }
+}
